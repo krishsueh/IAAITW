8a6fa5dac12faa26fe58242e21abc9c1606bbda6
KrisHsueh <hbmanikin@gmail.com>
2023-06-05 11:46:07 +0800

    feat: News

---
+++ IAAITW/Controllers/NewsController.cs
@@ -0,0 +1,51 @@
+﻿using IAAITW.Models;
+using MvcPaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IAAITW.Controllers
+{
+    public class NewsController : Controller
+    {
+        private IaaiTwDb db = new IaaiTwDb();
+        private const int DefaultPageSize = 5;
+
+        // GET: News
+        public ActionResult Index(int? page)
+        {
+            var breadcrumb = new List<ViewModel.BreadcrumbsItem>();
+            breadcrumb.Add(new ViewModel.BreadcrumbsItem { Text = "訊息發佈", Url = null });
+            breadcrumb.Add(new ViewModel.BreadcrumbsItem { Text = "訊息發佈", Url = Url.Action("Index", "News") });
+            ViewBag.Breadcrumb = breadcrumb;
+
+            int currentPageIndex = page.HasValue ? page.Value - 1 : 0;
+
+            // 排序由最新到最舊
+            return View(db.News.OrderByDescending(p => p.ReleaseDate).ToPagedList(currentPageIndex, DefaultPageSize));
+        }
+
+        // GET: News/Details/5
+        public ActionResult Details(int? id)
+        {
+            var breadcrumb = new List<ViewModel.BreadcrumbsItem>();
+            breadcrumb.Add(new ViewModel.BreadcrumbsItem { Text = "訊息發佈", Url = null });
+            breadcrumb.Add(new ViewModel.BreadcrumbsItem { Text = "訊息發佈", Url = Url.Action("Index", "News") });
+            ViewBag.Breadcrumb = breadcrumb;
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+    }
+}
